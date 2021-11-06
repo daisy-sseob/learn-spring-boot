c29c41fc00c870bb97fab469db465db9210148a7
hyun-sseob <orolsyeo@gmail.com>
2021-11-06 16:29:21 +0900

    🧑🏻‍💻test properties를 통해 test code 작성

--- src/test/java/me/sseob/learnspringboot/SimpleRunnerTest.java
+++ src/test/java/me/sseob/learnspringboot/SimpleRunnerTest.java
@@ -1,15 +1,17 @@
 package me.sseob.learnspringboot;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:/test.properties") //테스트용 properties로 테스트하자
 @SpringBootTest
 public class SimpleRunnerTest {
 
@@ -19,6 +21,6 @@ public class SimpleRunnerTest {
 	@Test
 	public void contextLoadsTest(){
 		assertThat(environment.getProperty("sseob.name"))
-				.isEqualTo("hyunseob");
+				.isEqualTo("hyun");
 	}
 }
