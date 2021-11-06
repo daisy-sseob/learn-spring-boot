05932ca406167f947dadf5ee80c5a69c7b29422b
hyun-sseob <orolsyeo@gmail.com>
2021-11-06 16:48:14 +0900

    property의 특정 key를 bean으로 만들어 type safe한 작업을 하는 방법. 🕶

--- src/main/java/me/sseob/learnspringboot/SimpleRunner.java
+++ src/main/java/me/sseob/learnspringboot/SimpleRunner.java
@@ -1,23 +1,35 @@
 package me.sseob.learnspringboot;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleRunner implements ApplicationRunner {
+
+	private final SeobProperties seobProperties;
+
+	@Autowired
+	public SimpleRunner(SeobProperties seobProperties) {
+		this.seobProperties = seobProperties;
+	}
 	
+	/*
+	@value 어노테이션이 아닌 Properties를 매핑하는 Class를 만들어 bean으로 주입받아 사용헤보자. 
 	@Value("${sseob.name}")
 	private String name;
-	
+
 	@Value("${sseob.age}")
 	private int age;
+	 */
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		System.out.println("============SimpleRunner============");
-		System.out.println("name: " + name + ", age: " + age);
+		System.out.println("name: " + seobProperties.getName());
+		System.out.println("full name: " + seobProperties.getFullName());
+		System.out.println("age: " + seobProperties.getAge());
 		System.out.println("============SimpleRunner============");
 		System.out.println("\n");
 	}
