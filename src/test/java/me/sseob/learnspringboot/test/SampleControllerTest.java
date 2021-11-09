572d1f47f19d58e2363b09b315c9068961b89b7f
hyun-sseob <orolsyeo@gmail.com>
2021-11-09 20:56:49 +0900

    add test

--- src/test/java/me/sseob/learnspringboot/test/SampleControllerTest.java
+++ src/test/java/me/sseob/learnspringboot/test/SampleControllerTest.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
