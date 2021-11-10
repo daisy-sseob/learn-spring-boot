19118a3228dd5e12152eacc1e1f19a805d16aa89
hyun-sseob <orolsyeo@gmail.com>
2021-11-10 19:43:13 +0900

    OutputCapture 추가

--- src/main/java/me/sseob/learnspringboot/test/SampleController.java
+++ src/main/java/me/sseob/learnspringboot/test/SampleController.java
@@ -1,11 +1,15 @@
 package me.sseob.learnspringboot.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
 
 	private final SampleService sampleService;
 
@@ -16,6 +20,10 @@ public class SampleController {
 
 	@GetMapping("/hello")
 	public String hello() {
+
+		log.debug("sseobs");
+		log.debug("=====");
+		
 		return "hello " + sampleService.getName();
 	}
 }
--- src/test/java/me/sseob/learnspringboot/test/SampleControllerSliceTest.java
+++ src/test/java/me/sseob/learnspringboot/test/SampleControllerSliceTest.java
@@ -1,14 +1,17 @@
 package me.sseob.learnspringboot.test;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,6 +19,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerSliceTest {
+
+	@Rule
+	public OutputCaptureRule outputCapture = new OutputCaptureRule();
 
 	@MockBean
 	SampleService mockSampleService;
@@ -30,5 +36,11 @@ public class SampleControllerSliceTest {
 		mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
 				.andExpect(status().isOk())
 				.andExpect(content().string("hello sseob test"));
+
+		assertThat(outputCapture.toString())
+				.contains("sseobs")
+				.contains("=====")
+		;
+		
 	}
 }
